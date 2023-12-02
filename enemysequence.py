b07590b17426ee3eb322136d6fd6ce91b92b53b0
gcfsalvador <145944423+gcfsalvador@users.noreply.github.com>
2023-12-02 08:02:36 +0800

    Update enemysequence.py

--- enemysequence.py
+++ enemysequence.py
@@ -6,9 +6,10 @@ monsternames = ["Goblin", "Slime", "Skeleton", "Walking Corpse", "Muscle Meat", 
 titlemonsters = ["Type 0", "Type 1", "Type 2", "Type 3", "Type 4", "Type Nexus", "Type Unregistered", "Extincted"]
 
 def generate_monster():
-    print(random.choice(titlemonsters), random.choice(monsternames))
+    Entity.name = random.choice(titlemonsters) 
+    Entity.type = random.choice(monsternames)
 
-enemy_name = generate_monster
+generate_monster()
 
 #Fight selections
 slow_type_EXTRA("THEY HAVE FOUND YOU.")
